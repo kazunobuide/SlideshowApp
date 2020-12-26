f72c4c6c0a057eda7e53b7e342ecdd642289f25f
kazunobuide <idekazunobu@gmail.com>
2020-12-26 11:59:52 +0900

    写真を表示

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //バンドルした画像ファイルを読み込み
+    
+    @IBOutlet weak var play: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    var image1:UIImage!
+    var image2:UIImage!
+    
+    var flg = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        image1=UIImage(named:"img1")
+        image2=UIImage(named:"img2")
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tapped(_ sender: Any) {
+        if flg{
+            imageView.image=image1
+        }
+        else{
+            imageView.image=image2
+            flg=true
+        }
+    }
 }
-
